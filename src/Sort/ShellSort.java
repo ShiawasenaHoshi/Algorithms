ef0b898c9452d7526c6cfa511df60496c1514394
Vasily Rozhnikov <shiawasena.hoshi@gmail.com>
2015-06-19 20:15:59 +0500

    Сортировка Шелла готова Тесты сделаны автоматическими

--- src/Sort/ShellSort.java
+++ src/Sort/ShellSort.java
@@ -11,18 +11,68 @@ import java.util.List;
 public class ShellSort<T extends Comparable<T>> implements Sorter<T> {
     List<T> list;
     int size;
+    final private static int[] EMPIRIC_DELTAS = {701, 301, 132, 57, 23, 10, 4};
+
     @Override
     public Collection<T> sort(Collection<T> collection) {
-        if(collection.size() <= 1){
+        if (collection.size() <= 1) {
             return collection;
         }
         copyToArray(collection);
-        T buffer;
+        int currentDelta;
+        for (int currentDeltaIndex = 0; currentDeltaIndex < EMPIRIC_DELTAS.length; currentDeltaIndex++) {
+            currentDelta = EMPIRIC_DELTAS[currentDeltaIndex];
+            if (currentDelta >= size) {
+                continue;
+            }
+            for (int startIndex = 0; startIndex < currentDelta; startIndex++) {
+                int elementsAmount = ((size - startIndex) / currentDelta) + 1;
+                if (elementsAmount == 1) {
+                    break;
+                }
+                int[] indexesForPart = new int[elementsAmount];
+                for (int i = 0, indexForPart = startIndex; indexForPart < size; i++, indexForPart += currentDelta) {
+                    indexesForPart[i] = indexForPart;
+                }
+                List<T> part = new ArrayList<>();
+                for (int i : indexesForPart) {
+                    part.add(list.get(i));
+                }
+                insertSort(part);
+                for (int i = 0; i < part.size(); i++) {
+                    list.set(indexesForPart[i], part.get(i));
+                }
+            }
+        }
+        insertSort(list);
+        return list;
     }
 
-    private void copyToArray(Collection<T> collection){
+    private void copyToArray(Collection<T> collection) {
         list = new ArrayList<>();
         list.addAll(collection);
         size = list.size();
+    }
+
+    //Поскольку сортировка Шелла содержит сортировку вставками, сортировка вставками выделена в метод SortPart
+    private void insertSort(List<T> part) {
+        T buffer;
+        int partSize = part.size();
+        for (int i = 1; i < partSize; i++) {
+            T current = part.get(i);
+            int currentIndex = i;
+            int prevIndex = i - 1;
+            while (current.compareTo(part.get(prevIndex)) < 1) {
+                buffer = part.get(prevIndex);
+                part.set(prevIndex, current);
+                part.set(currentIndex, buffer);
+                if (prevIndex > 0) {
+                    prevIndex--;
+                    currentIndex--;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ test/Sort/SortTest.java
@@ -0,0 +1,55 @@
+package Sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static Sort.Strategy.sort;
+/**
+ * Created by vasily on 19.06.15.
+ */
+public class SortTest {
+
+    Integer[] integers = {
+            new Integer(5),
+            new Integer(4),
+            new Integer(3),
+            new Integer(10),
+            new Integer(8),
+            new Integer(9),
+            new Integer(7),
+            new Integer(2),
+            new Integer(6),
+            new Integer(1),
+            new Integer(0),
+    };
+    int[] expectedResult = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    @Test
+    public void testInsertSort() throws Exception {
+        Collection<Integer> sortedInts = sort(Arrays.asList(integers), new InsertSort<>());
+        checkCollection(sortedInts);
+    }
+
+    @Test
+    public void testShellSort() throws Exception {
+        Collection<Integer> sortedInts = sort(Arrays.asList(integers), new ShellSort<>());
+        checkCollection(sortedInts);
+    }
+
+    @Test
+    public void testSort1() throws Exception {
+
+    }
+
+    private void checkCollection(Collection<Integer> collection){
+        Iterator<Integer> it = collection.iterator();
+        for (int i = 0; i < expectedResult.length; i++) {
+            int integer = it.next();
+            Assert.assertEquals(expectedResult[i], integer);
+        }
+    }
+}
